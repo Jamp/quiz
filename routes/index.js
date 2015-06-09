119dad9d81ee5a250232307a51d1b79f7b3fe482
Jaro Marval <jampgold@gmail.com>
2015-06-08 20:20:42 -0430

    Agregando la característica borrar

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 
 // Objetivo 2 de la tarea
